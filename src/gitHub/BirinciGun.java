3a2470c973cee79767877ecc97a82bfa037ad513
Leyla Nart <leylanart85@gmail.com>
2021-03-02 09:23:42 -0800

    Ucuncuc version

--- src/gitHub/BirinciGun.java
+++ src/gitHub/BirinciGun.java
@@ -5,6 +5,7 @@ public class BirinciGun {
 	public static void main(String[] args) {
 		System.out.println("Birinci Gun");
 		System.out.println("ikincici Gun");
+		System.out.println("ucuncuci Gun");
 	}
 
 }
