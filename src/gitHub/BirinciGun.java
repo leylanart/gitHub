c4fa4b3e17e5180a5d0b7e89afc48e486a8d4002
leylanart <77584438+leylanart@users.noreply.github.com>
2021-03-02 09:46:41 -0800

    Update BirinciGun.java

--- src/gitHub/BirinciGun.java
+++ src/gitHub/BirinciGun.java
@@ -7,6 +7,8 @@ public class BirinciGun {
 		System.out.println("ikincici Gun");
 		System.out.println("ucuncuci Gun");
 		System.out.println("dorduncu Gun");
+		System.out.println("dorduncu Gun");
+			
 			
 	}
 
